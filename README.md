9af3d6c65e921c0b521ba3c6440a5c09b97ac44c
alcun <95560342+alcun@users.noreply.github.com>
2021-12-10 13:17:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+An automated lesson report generator project.
+
+Made using python with features such as if/else loop, branches and nesting.
+
+Purpose is to help teachers save time writing up reports after lessons while still being able to give useful lesson recap info and areas to improve upon
+
+
+CURRENT VERSION:
+Skeleton of project
+
+TO DO:
+Fill in action and content for modifiers: name, age, effort1-3, ability1-5, subjects 1-3 + comment, improvements, generic script, ui friendly output
---
+++ main.py
@@ -0,0 +1,52 @@
+
+print("Hello, welcome to the automatic report generator")
+
+name
+input("What was the name of your student? (Name): ")
+
+meeting/learning
+input("Was it your first time teaching them? (yes/no): ")
+
+ability
+input("What is the level of their ability (1(novice) - 5(fluent): ") 
+
+effort level
+input("What was their level of effort (1(asleep) - 3(very engaged): ")
+
+
+
+
+subject of class
+input("What is one thing you discussed in class? (1): ")
+input("comment (1): ")
+input("What is another thing you discussed in class? (2): ")
+input("comment (2): ")
+input("What was the final thing you discussed in class? (3): ")
+input("comment(3): ")
+
+
+print("Brilliant job (name)!")
+
+
+improvements to be made 
+input("Where can the student improve"?)
+
+input("Any additional comments?")
+
+
+print(Have an excellent day :))
+
+print(final_report) in html 
+
+
+
+'''template'''
+
+Hi (name). 
+
+meet: It was nice to meet you.
+notmeet: It was great to learn with you today 
+
+
+Well done on some (level of effort) work in class today. Your concentration and effort levels were (level of effort)
+In our lesson, we looked at 
